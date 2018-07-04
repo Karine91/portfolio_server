ec1dd5b5a827c74f3682814d52ada6c169b24b2f
karine <kitana.91@mail.ru>
2018-07-04 20:42:28 +0300

    updates

--- app.js
+++ app.js
@@ -46,7 +46,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://karine-portfolio.herokuapp.com, http://localhost:3000");
+  var allowedOrigins = ['https://karine-portfolio.herokuapp.com', 'http://localhost:3000'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
