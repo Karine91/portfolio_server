ef89986424f55e223d4a7523e69cd46378333e3d
karine <kitana.91@mail.ru>
2018-07-22 14:57:33 +0300

    fix

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,5 @@
 module.exports.getAdminpage = function (req, res) {
-    res.render('admin/admin', { 
+    res.render('pages/admin', { 
         title: 'Админ панель' ,
         msgfile: req.query.msgfile,
         msgblog: req.query.msgblog,
